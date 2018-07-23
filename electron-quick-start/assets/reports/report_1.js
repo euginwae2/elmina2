f4f67f9e54432386aa24963326a4dcbfb1076def
euginwae <euginwae@live.com>
2018-07-23 14:05:33 +0000

    pdf printing
    Added pdf printing feature

--- electron-quick-start/assets/reports/report_1.js
+++ electron-quick-start/assets/reports/report_1.js
@@ -11,8 +11,9 @@ const Table  = require(currentPath);
 const modelPath = path.join( __dirname, '../models')
 models = require(modelPath);
 let page = document.getElementById('reportContainer');
+var ipcRenderer = require('electron');
 
-
+addControlBar();
 var newTable = new Table();
 
 models.Field.findAll({order: [['id' , 'ASC']]})
@@ -31,102 +32,28 @@ models.Field.findAll({order: [['id' , 'ASC']]})
             {
                 newTable.addCell(x[key])
             }
-            //console.log(x.dataValues);
-            /* Array.prototype.forEach.call(x.dataValues,(y) => {
-                console.log(y);
-            }) */
         })
     })
 )
 
 page.appendChild(newTable.getTable());
- 
-/* 
 
-let container = document.getElementById('reportContainer');
-
-getAssetDispNames()
-.then((res) => {
-    console.log(res);
-    buildHeader(res)
-    .then((results) => {
-        container.appendChild(results);
-        
+function addPrintButton(parent)
+{
+    let printBtn = document.createElement('button');
+    printBtn.innerHTML = 'Print To PDF';
+    printBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        const {ipcRenderer} = require('electron');
+        ipcRenderer.send('print-to-pdf');
     })
+    parent.appendChild(printBtn);
+}
 
-});
-models.Asset.findAll({order: [['id' , 'ASC']]})
-.then((res) => {
-    console.log(res);
-    buildRow(res)
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((error) => {console.log(error);})
-});
- */
-
-// function getAssetDispNames () {
-//     return new Promise(function(resolve, reject)
-//         {
-//     /**
-//      * @description : get the displayd names of asset fields
-//      */
-//     var keys = [];
-//      models.Field.findAll()
-//      .then((res) => {
-//          Array.prototype.forEach.call(res, (x) => {
-//             keys.push(x.name);
-//          }) 
-//      })
-//      .then(() => {resolve(keys)})
-//      .catch((error) => { reject(error)});
-//     })
-// }
-
-// function buildHeader (args) {
-//     /**
-//      * @description takes table column names and returns table title
-//      */
-
-//     return new Promise(function(resolve,reject) {
-//         let titleBar = document.createElement('tr');
-//         //titleBar.setAttribute('class', 'col-6');
-//         Array.prototype.forEach.call(args.sort(),(y)=>{
-//             let title = document.createElement('th');
-//             title.appendChild(document.createTextNode(y));
-//             //title.setAttribute('class', 'title');
-//             titleBar.appendChild(title);  
-//         })
-//         resolve(titleBar);
-//     })
-    
-// }
-
-// function buildRow (args) {
-//     /**
-//      *@description Takes asset rows as imputs and returns as table rows
-//      */
-//     return new Promise (function (resolve, reject) {
-        
-//         console.log('size of args : ', args.length);
-//         console.log('passed argument : ', args);
-//         Array.prototype.forEach.call(args,(arg) => {
-//             var rowContainer = document.createElement('tr');
-            
-//            // arg.dataValues
-//             //console.log(arg.dataValues);
-//              Array.prototype.forEach.call(arg.dataValues, (field) => {
-//                 //var keys = Object.keys(field.dataValues);
-//                 console.log(field);
-//                 /* var row = document.createElement('td');
-//                 row.appendChild(document.createTextNode(field.dataValues[keys]));
-//                 console.logo(row); */
-//             })
-//             //rowContainer.appendChild(row);
-//             //console.log(rowContainer); */
-            
-//         })
-//         //resolve(rowContainer);
-//     })
-// }
+function addControlBar()
+{
+    let cntlBar = document.createElement('div');
+    cntlBar.setAttribute('class', 'col-6');
+    addPrintButton(cntlBar);
+    page.appendChild(cntlBar);
+}
--- electron-quick-start/main-process/report-view.js
+++ electron-quick-start/main-process/report-view.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const {ipcMain,BrowserWindow,shell} = require('electron')
 
 
-  
+ ipcMain.on('print-to-pdf',(event) => {
+     const win = BrowserWindow.fromWebContents(event.sender);
+     const pdfPath = path.join(os.tmpdir(),'print-pdf.pdf');
+     win.webContents.printToPDF({},(error,data) => {
+        if (error) throw error
+        fs.writeFile(pdfPath, data, (error) => {
+            if (error) throw error
+            const storeLocation  = path.join('file://',pdfPath);
+            shell.openExternal(storeLocation)
+        })
+     })
+ }) 
